2c6dc3343c36599b9aaba57f67a080ab1310cf18
bansaljatin05 <58761492+bansaljatin05@users.noreply.github.com>
2020-08-02 00:14:45 +0530

    Small Change

--- models/meals.js
+++ models/meals.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const mealSchema = new mongoose.Schema({
-    day: String,
-    time: String,
-    itemName: String
+    day: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    itemName: {
+        type: String,
+        required: true
+    }
 });
 
 const Meals = mongoose.model("Meal", mealSchema);
